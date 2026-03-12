7517dfa673d3df378f721bcd1ee24c58366e1c7f
scssw <90193529+scssw@users.noreply.github.com>
2026-03-12 15:23:41 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -28,7 +28,7 @@ let 临时中转域名 = [];
 let 临时中转域名接口 = '';
 let EndPS = '';
 let 协议类型 = atob(`\u0056\u006b\u0078\u0046\u0055\u0031\u004d\u003d`);
-let FileName = '优选订阅生成器';
+let FileName = '动态多线加速';
 let SUBUpdateTime = 6;
 let total = 24;
 let timestamp = 4102329600000;
@@ -1463,3 +1463,4 @@ export default {
 		}
 	}
 };
+
